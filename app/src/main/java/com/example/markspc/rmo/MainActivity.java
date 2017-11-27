7016db59faf1051d0cd0d3bb830d715eb0eedb72
walshey1117 <34000527+walshey1117@users.noreply.github.com>
2017-11-27 15:57:09 +0000

    Login and MainActivity

--- app/src/main/java/com/example/markspc/rmo/MainActivity.java
+++ app/src/main/java/com/example/markspc/rmo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.markspc.rmo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Log Out", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -80,17 +81,18 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
+            Intent myIntent = new Intent(MainActivity.this, MainActivity.class);
+            startActivityForResult(myIntent, 0);
+        } else if (id == R.id.nav_clockin) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_clockout) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_locations) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_handbook) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_conditions) {
 
         }
 
