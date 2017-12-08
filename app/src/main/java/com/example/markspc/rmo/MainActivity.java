94ddbf61179124bbc904793aa4039be109fa3bd2
walshey1117 <34000527+walshey1117@users.noreply.github.com>
2017-12-08 19:21:19 +0000

    Login connected to mysql data base and MainActivity with fragments

--- app/src/main/java/com/example/markspc/rmo/MainActivity.java
+++ app/src/main/java/com/example/markspc/rmo/MainActivity.java
@@ -1,21 +1,34 @@
 package com.example.markspc.rmo;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +37,9 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Log Out", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        pref = this.getPreferences(0);
+        initFragment();
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -42,6 +50,19 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
+
+    private void initFragment(){
+        Fragment fragment;
+        if(pref.getBoolean(Constants.IS_LOGGED_IN,false)){
+            fragment = new ProfileFragment();
+        }else {
+            fragment = new LoginFragment();
+        }
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_frame,fragment);
+        ft.commit();
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -69,7 +90,13 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            Fragment fragment;
+            fragment = new LoginFragment();
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.replace(R.id.fragment_frame,fragment);
+            ft.commit();
+
+
         }
 
         return super.onOptionsItemSelected(item);
@@ -90,9 +117,15 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_locations) {
 
-        } else if (id == R.id.nav_handbook) {
+        }else if (id == R.id.nav_profile) {
 
-        } else if (id == R.id.nav_conditions) {
+        }else if (id == R.id.nav_handbook) {
+            Fragment fragment;
+            fragment = new LoginFragment();
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.replace(R.id.fragment_frame,fragment);
+            ft.commit();
+        }else if (id == R.id.nav_conditions) {
 
         }
 
